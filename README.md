d9c2b7c0dbad58670567c58590b6d675cf4fd0d1
joname1 <274158741@qq.com>
2018-05-17 14:51:41 +0800

    ALL SORTS OF THINGS

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wxapp-Loading
+微信小程序Loading，不断更新中...
+
+# Usage
+
+* 全局调用 loading.wxss
+
+* 在页面插入`<view>` 详情见文件 index.wxml
+
+# Demo
+* PS: 在真机预览的画面是非常流畅的
+
+![](demo.gif)
---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    
+  },
+  //事件处理函数
+  bindViewTap: function() {
+
+  },
+  onLoad: function () {
+    console.log('onLoad')
+  }
+})
